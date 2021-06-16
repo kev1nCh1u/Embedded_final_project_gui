a5a3477816677fdcae25f177d4e077f4426640f8
KevinChiu <fsd83811@gmail.com>
2021-06-16 20:14:20 +0800

    cached

---
+++ src/joystick.py
@@ -0,0 +1,110 @@
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+from enum import Enum
+import math
+
+
+class Direction(Enum):
+    Left = 0
+    Right = 1
+    Up = 2
+    Down = 3
+
+
+class Joystick(QWidget):
+    def __init__(self, parent=None):
+        super(Joystick, self).__init__(parent)
+        self.setMinimumSize(150, 150)
+        self.movingOffset = QPointF(0, 0)
+        self.grabCenter = False
+        self.__maxDistance = 50
+        self.joyPosXY = (0, 0)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        bounds = QRectF(-self.__maxDistance, -self.__maxDistance,
+                        self.__maxDistance * 2, self.__maxDistance * 2).translated(self._center())
+        painter.drawEllipse(bounds)
+        painter.setBrush(Qt.black)
+        painter.drawEllipse(self._centerEllipse())
+
+    def _centerEllipse(self):
+        if self.grabCenter:
+            return QRectF(-20, -20, 40, 40).translated(self.movingOffset)
+        return QRectF(-20, -20, 40, 40).translated(self._center())
+
+    def _center(self):
+        return QPointF(self.width()/2, self.height()/2)
+
+    def _boundJoystick(self, point):
+        limitLine = QLineF(self._center(), point)
+        if (limitLine.length() > self.__maxDistance):
+            limitLine.setLength(self.__maxDistance)
+        return limitLine.p2()
+
+    def joystickDirection(self):
+        if not self.grabCenter:
+            return (0, 0)
+        normVector = QLineF(self._center(), self.movingOffset)
+        currentDistance = normVector.length()
+        angle = normVector.angle()
+        x = currentDistance * math.cos(math.radians(angle))
+        y = currentDistance * math.sin(math.radians(angle))
+        return (x, y)
+        # return (currentDistance, angle)
+
+        # distance = min(currentDistance / self.__maxDistance, 1.0)
+        # if 45 <= angle < 135:
+        #     return (Direction.Up, distance)
+        # elif 135 <= angle < 225:
+        #     return (Direction.Left, distance)
+        # elif 225 <= angle < 315:
+        #     return (Direction.Down, distance)
+        # return (Direction.Right, distance)
+
+    def mousePressEvent(self, ev):
+        self.grabCenter = self._centerEllipse().contains(ev.pos())
+        return super().mousePressEvent(ev)
+
+    def mouseReleaseEvent(self, event):
+        self.grabCenter = False
+        self.movingOffset = QPointF(0, 0)
+        self.update()
+        self.joyPosXY = (0, 0)
+
+    def mouseMoveEvent(self, event):
+        if self.grabCenter:
+            # print("Moving")
+            self.movingOffset = self._boundJoystick(event.pos())
+            self.update()
+        self.joyPosXY = self.joystickDirection()
+        # print(self.joyPosXY)
+
+
+if __name__ == '__main__':
+    # Create main application window
+    app = QApplication([])
+    app.setStyle(QStyleFactory.create("Cleanlooks"))
+    mw = QMainWindow()
+    mw.setWindowTitle('Joystick example')
+
+    # Create and set widget layout
+    # Main widget container
+    cw = QWidget()
+    ml = QGridLayout()
+    cw.setLayout(ml)
+    mw.setCentralWidget(cw)
+
+    # Create joystick
+    joystick = Joystick()
+
+    # ml.addLayout(joystick.get_joystick_layout(),0,0)
+    ml.addWidget(joystick, 0, 0)
+
+    mw.show()
+
+    # Start Qt event loop unless running in interactive mode or using pyside.
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QApplication.instance().exec_()
---
+++ src/kevin_serial.py
@@ -0,0 +1,132 @@
+import serial
+import glob
+import sys
+
+def serial_ports():
+    """ Lists serial port names
+ 
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/ttyUSB*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+ 
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+class SerialFuc():
+    def __init__(self, port='nan'):
+        super().__init__()
+        if port == 'nan':
+            self.ser = serial.Serial()
+            self.ser.baudrate = 115200
+            self.ser.timeout = 0.1
+        else:
+            self.ser = serial.Serial(port, 115200, timeout=0.1)  # open serial port
+            # print(self.ser.name)         # check which port was really used
+
+    def changePort(self, port):
+        msg = ''
+        msg = self.ser.close()
+        self.ser.port = port
+        msg = self.open()
+        # print(self.ser.name)         # check which port was really used
+        # print(self.ser)
+        return msg
+        
+
+    def read(self):
+        data = (0,0)
+
+        if self.ser.is_open:
+            while 1:
+                self.ser.write(chr(71).encode())
+                
+                buff = self.ser.read().decode()        # read one byte
+                # print(buff)
+                # print(self.ser.readline())
+
+                if buff != '':
+                    buff = ord(buff)
+                # print(buff)
+                if buff == 83:
+                    break
+
+            x = ord(self.ser.read().decode('utf-8'))        # read one byte
+            y = ord(self.ser.read().decode('utf-8'))        # read one byte
+            data = (x, y)
+
+        # print(data)
+
+        return data
+    
+    def write(self, mode, x=0, y=0):
+        if self.ser.is_open:
+            # print(x,y)
+            self.ser.write(chr(67).encode())
+            if mode == 'R':
+                self.ser.write(chr(82).encode())
+            elif mode == 'V':
+                self.ser.write(chr(86).encode())
+                self.ser.write(chr(x).encode())
+                self.ser.write(chr(y).encode())
+                # print(self.ser.readline())
+
+    def portStatus(self):
+        return self.ser.is_open
+
+    def open(self, port=''):
+        msg = ''
+        # print(self.ser)
+        if port != '' and port != self.ser.port:
+            msg = self.ser.close()
+            self.ser.port = port
+        try:
+            if self.ser.name != None:
+                self.ser.open()             # open port
+                msg = 'Connect'
+            else:
+                msg = 'no port set'
+        except:
+            if self.ser.is_open == True:
+                msg = 'Already connect'
+            else:
+                msg = 'Error can not connect !!!'
+        # print(msg)
+        return msg
+
+    def close(self):
+        msg = ''
+        self.ser.close()             # close port
+        msg = 'Disconnect'
+        # print(msg)
+        return msg
+
+
+if __name__ == '__main__':
+    ports = serial_ports()
+    for i in range(len(ports)):
+        print(i, ':', ports[i], ' ', sep='')
+
+    # port_num = int(input('Please chose a port(0,1,2...): '))
+
+    ser = SerialFuc(ports[0])
+    while 1:
+        # input()
+        print(ser.read())
